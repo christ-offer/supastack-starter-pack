cc5ed53991c7a188069ba194855bb30b3443cfb7
Chris Holst <cholst@gmail.com>
2024-11-29 12:25:38 +0100

    unneeded queery

--- migrations/020-components-header.sql
+++ migrations/020-components-header.sql
@@ -49,12 +49,10 @@ create or replace function public.get_header()
 returns "text/html" as $$
 declare
   user_id uuid := auth.uid ();
-  user_profile public.profiles;
 begin
   if user_id is null then
     return public.get_unauth_header();
   end if;
-  select * into user_profile from public.profiles where id = user_id;
   return public.get_auth_header();
 end;
 $$ language plpgsql;
